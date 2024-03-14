880529cdee04c2caf168591a9841692d09a3eb53
Lee Jaewoo <evisahah@gmail.com>
2024-03-14 23:58:00 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "재우씨",
     image: "/avatar3.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "웹 개발자(일병)",
-    bio: "재우씨, 이거 이렇게 하면 안돼요.🤬",
+    bio: "재우씨, 이거 이렇게 하면 안돼요.",
     email: "evisahah@gmail.com",
     linkedin: "",
     github: "LeeDiculous",
